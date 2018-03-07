45b3e4732739afd329596d602dd8d561f0e7cff0
User <user@Users-MacBook-Pro.local>
2018-03-07 18:14:10 +1300

    made invisible div visible

--- classes.js
+++ classes.js
@@ -7,7 +7,8 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
    one()
    two()
-    
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -27,4 +28,12 @@ two.classList.add('green')
 
 // CREATE FUNCTION three HERE
 
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('red')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')
+  makeVisible[0].classList.add('visible')
+}
